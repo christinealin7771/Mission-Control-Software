7ccc82e72f58bd885b79c1a0da10f568e3f67dcc
SusBhh <susanab20020911@gmail.com>
2022-02-22 15:17:55 -0500

    get

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Graphs from './components/Graphs';
-
+import './assets/lovelyChristineLin.jpg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,7 +11,9 @@ function App() {
     <div className="App">
       <Router>
         <Header/>
-        Hello
+        Hello! Here is a lovely picture of Christine Lin as place holder for yall! 
+        <br/>
+        <img src='https://media-exp1.licdn.com/dms/image/C4D03AQGkvpt9zXecTA/profile-displayphoto-shrink_200_200/0/1602626520962?e=1649894400&v=beta&t=EYqxA6tpD-baPRPR_xZa8FbpSHLjThxFynO6HihJ82A'></img>
           <Routes>
           <Route path="/graphs" element={<Graphs/>}></Route>
           </Routes>
