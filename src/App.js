c97bb786ed6f49f8f7bbf9c95a009cd61d10843d
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2022-03-14 18:20:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Graphs from './components/Graphs';
-import './assets/lovelyChristineLin.jpg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header/>
-        Hello! Here is a lovely picture of Christine Lin as place holder for yall! 
-        <br/>
-        <img src='https://media-exp1.licdn.com/dms/image/C4D03AQGkvpt9zXecTA/profile-displayphoto-shrink_200_200/0/1602626520962?e=1649894400&v=beta&t=EYqxA6tpD-baPRPR_xZa8FbpSHLjThxFynO6HihJ82A'></img>
-          <Routes>
-          <Route path="/graphs" element={<Graphs/>}></Route>
-          </Routes>
-        <Footer/>
-      </Router>
+    
      
     </div>
   );
